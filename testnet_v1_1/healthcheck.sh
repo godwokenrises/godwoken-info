ae166613e2105987fbfdeea8c85cca2361ae797d
Flouse <flouse@gmail.com>
2022-05-30 08:16:44 +0800

    fix(sync-workflow): check the ckb-indexer TIP of testnet

--- testnet_v1_1/healthcheck.sh
+++ testnet_v1_1/healthcheck.sh
@@ -1,5 +1,28 @@
 #!/usr/bin/env bash
 
+function get_ckb_indexer() {
+  local ckb_indexer_url=$1
+  echo '{
+    "id": 2,
+    "jsonrpc": "2.0",
+    "method": "get_tip"
+  }' \
+  | tr -d '\n' \
+  | curl -sS -H 'content-type: application/json' -d @- \
+  $ckb_indexer_url \
+  | awk 'BEGIN { FS=":"; RS="," }; { if ($1 == "\"block_number\"") {print $2} }' \
+  | tr -d '}'
+}
+
+function check_ckb_indexer_tip() {
+  local local_indexer_tip="$(get_ckb_indexer http://ckb-testnet-indexer:8116)"
+  local remote_indexer_tip="$(get_ckb_indexer https://testnet.ckbapp.dev/indexer)"
+  test $local_indexer_tip = $remote_indexer_tip
+}
+
+# Check the ckb-indexer TIP of testnet
+check_ckb_indexer_tip
+
 # Query whether a Godwoken readonly node is ready to serve
 # see: https://github.com/nervosnetwork/godwoken/pull/644
 echo '{
