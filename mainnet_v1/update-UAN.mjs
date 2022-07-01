2726baad8d645f7a92849f0f60266146c6b97978
Flouse <flouse@gmail.com>
2022-07-01 11:53:07 +0800

    mainnet_v1: fix UAN of bridged tokens

--- mainnet_v1/update-UAN.mjs
+++ mainnet_v1/update-UAN.mjs
@@ -7,8 +7,13 @@
  */
 
 // Get ForceBridge uan-token-list
-let resp = await fetch('https://raw.githubusercontent.com/nervosnetwork/force-bridge/20f25902d2f86e54585881b53c62a5ec42da5e1a/configs/uan-token-list.json')
-const forceBridgeTokenList = await resp.json();
+let forceBridgeTokenList = await fetch('https://raw.githubusercontent.com/nervosnetwork/force-bridge/20f25902d2f86e54585881b53c62a5ec42da5e1a/configs/uan-token-list.json')
+  .then(resp => resp.json())
+  .catch(e => {
+    console.log(e);
+    const list = require("./fb.json");
+    return list;
+});
 
 let sudtMap = new Map();
 forceBridgeTokenList.forEach(token => sudtMap.set(token.sudtArgs, token));
@@ -17,7 +22,16 @@ let gwBridgedTokenList = require("./bridged-token-list.json");
 gwBridgedTokenList.forEach(token => {
   if (!sudtMap.has(token.erc20Info.sudtScriptArgs)) return;
 
-  token.UAN = sudtMap.get(token.erc20Info.sudtScriptArgs).uanInfo.bridgedUan;
+  const fbToken = sudtMap.get(token.erc20Info.sudtScriptArgs);
+  const symbol = fbToken.uanInfo.sourceDisplayName;
+  
+  // assert
+  if (!token.info.symbol.includes(symbol)) {
+    throw new Error(`Symbol ${symbol} not match`);
+  }
+
+  let ckbUan = fbToken.uanInfo.bridgedUan;
+  token.UAN = `${symbol}.gw|gb${ckbUan.substring(symbol.length)}`;
   token.displayName = sudtMap.get(token.erc20Info.sudtScriptArgs).uanInfo.bridgedDisplayName;
 })
 
